81f4152144646675254269348eeb3b63c9b96370
jungpaeng <meis1541@naver.com>
2019-03-05 00:22:04 +0900

    :sparkles: Connect Peer to Each Others

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const BlockChain = require('./blockchain');
 const P2P = require('./p2p');
 
 const { getBlockChain, createNewBlock } = BlockChain;
-const { startP2PServer } = P2P;
+const { startP2PServer, connectToPeers } = P2P;
 
 const PORT = process.env.HTTP_PORT || 3000;
 
@@ -22,6 +22,12 @@ app.post('/block', (req, res) => {
   const { body: { data } } = req;
   const newBlock = createNewBlock(data);
   res.send(newBlock);
+});
+
+app.post('/peers', (req, res) => {
+  const { body: { peer } } = req;
+  connectToPeers(peer);
+  res.send();
 });
 
 const server = app.listen(PORT, () => {
