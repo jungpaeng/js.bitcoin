ac72587dad6a4d93eb68980f226a212b470803ba
jungpaeng <meis1541@naver.com>
2019-03-07 10:19:15 +0900

    :sparkles: Create Error Handling Function

--- src/p2p.js
+++ src/p2p.js
@@ -4,8 +4,17 @@ const sockets = [];
 
 const getSockets = () => sockets;
 
+const handleSocketError = (ws) => {
+  const closeSocketConnection = (ws) => {
+    ws.close();
+    sockets.splice(sockets.indexOf(ws), 1);
+  };
+  ws.on('error', () => closeSocketConnection(ws));
+};
+
 const initSocketConnection = (socket) => {
   sockets.push(socket);
+  handleSocketError(socket);
   socket.on('message', (data) => {
     console.log(data);
   });
