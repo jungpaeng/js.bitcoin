249f0c97c2633098afe68873e882bc6c94168e27
jungpaeng <meis1541@naver.com>
2019-03-10 02:02:57 +0900

    :sparkles: Get Address and Address Balance

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,7 @@
 const EC = require('elliptic').ec;
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
 
 const ec = new EC('secp256k1');
 
@@ -11,6 +12,22 @@ const generatePrivateKey = () => {
   const privateKey = keyPair.getPrivate();
   return privateKey.toString(16);
 };
+
+const getPrivateFromWallet = () => {
+  const buffer = fs.readFileSync(privateKeyLocation, 'utf-8');
+  return buffer.toString();
+};
+
+const getPublicFromWallet = () => {
+  const privateKey = getPrivateFromWallet();
+  const key = ec.keyFromPrivate(privateKey, 'hex');
+  return key.getPublic().encode('hex');
+};
+
+const getBalance = (address, uTxOuts) => _(uTxOuts)
+  .filter(uTxO => uTxO.address === address)
+  .map(uTxO => uTxO.amount)
+  .sum();
 
 const initWallet = () => {
   if (!fs.existsSync(privateKeyLocation)) {
