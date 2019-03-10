558de9bb706c476d74ebb430799e12dc6e4b0c0c
jungpaeng <meis1541@naver.com>
2019-03-10 13:58:02 +0900

    :sparkles: Create Coinbase Transaction

--- src/transaction.js
+++ src/transaction.js
@@ -250,6 +250,17 @@ const validateCoinbaseTx = (tx, blockIndex) => {
   return true;
 };
 
+const createCoinbaseTx = (address, blockIndex) => {
+  const tx = new Transaction();
+  const txIn = new TxIn();
+  txIn.signature = '';
+  txIn.txOutId = blockIndex;
+  tx.txIns = [txIn];
+  tx.txOuts = [new TxOut(address, COINBASE_AMOUNT)];
+  tx.id = getTxId(tx);
+  return tx;
+};
+
 module.exports = {
   Transaction,
   TxIn,
@@ -257,4 +268,5 @@ module.exports = {
   getPublicKey,
   getTxId,
   signTxIn,
+  createCoinbaseTx,
 };
